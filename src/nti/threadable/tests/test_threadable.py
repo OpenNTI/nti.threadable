e3e85bcad3fa546b9567868e2a2252deb46fc0f3
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-02-14 22:56:29 +0000

    add initial test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.threadable/trunk@106545 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/threadable/tests/test_threadable.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import assert_that
+
+from nti.testing.matchers import validly_provides
+from nti.testing.matchers import verifiably_provides
+
+import unittest
+
+from nti.threadable.interfaces import IThreadable
+
+from nti.threadable.threadable import Threadable
+
+from nti.threadable.tests import SharedConfiguringTestLayer
+
+
+class TestThreadable(unittest.TestCase):
+
+    layer = SharedConfiguringTestLayer
+
+    def test_interface(self):
+        threadable = Threadable()
+        assert_that(threadable, validly_provides(IThreadable))
+        assert_that(threadable, verifiably_provides(IThreadable))
