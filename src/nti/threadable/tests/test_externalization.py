1c79c956d9bba65310a8b6b4a77b3da75d03fce0
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-10-05 16:28:13 +0000

    86%
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.threadable/trunk@122785 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/threadable/tests/test_externalization.py
+++ src/nti/threadable/tests/test_externalization.py
@@ -12,7 +12,9 @@ from hamcrest import is_
 from hamcrest import none
 from hamcrest import is_not
 from hamcrest import has_entry
+from hamcrest import has_length
 from hamcrest import assert_that
+from hamcrest import has_property
 
 import fudge
 import unittest
@@ -64,9 +66,11 @@ class TestExternalization(unittest.TestCase):
 
         # don't write missing references
         context = P()
+
         class NoneRef(object):
             def __conform__(self, unused):
                 return None
+
             def __call__(self):
                 return None
         pio = PInternalObjectIO(context)
@@ -75,22 +79,34 @@ class TestExternalization(unittest.TestCase):
         context._inReplyTo = NoneRef()
         assert_that(pio.toExternalObject(),
                     has_entry('inReplyTo', is_(none())))
-        
+
         # write missing references
         pio._ext_write_missing_references = True
         assert_that(pio.toExternalObject(),
                     has_entry('inReplyTo', is_(none())))
-        
+
         # write missing ntiid
         class MissingNTIIDRef(object):
             def __conform__(self, unused):
                 return self
+
             def __call__(self):
                 return None
+
             def make_missing_ntiid(self):
                 return "missing"
 
         context._inReplyTo = MissingNTIIDRef()
         assert_that(pio.toExternalObject(),
                     has_entry('inReplyTo', 'missing'))
-        
+
+    def test_import(self):
+        context = P()
+        assert_that(context, has_property('inReplyTo', is_(none())))
+        assert_that(context, has_property('references', is_(())))
+
+        pio = PInternalObjectIO(context)
+        pio.updateFromExternalObject({'inReplyTo':  P(),
+                                      'references': [P()]})
+        assert_that(context, has_property('inReplyTo', is_not(none())))
+        assert_that(context, has_property('references', has_length(1)))
