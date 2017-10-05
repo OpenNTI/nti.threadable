773436d77df24b75f204abf3d9309b24e9c938f6
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-10-05 16:45:29 +0000

    88%
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.threadable/trunk@122787 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/threadable/tests/test_threadable.py
+++ src/nti/threadable/tests/test_threadable.py
@@ -8,12 +8,19 @@ from __future__ import absolute_import
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
+from hamcrest import is_
+from hamcrest import none
 from hamcrest import assert_that
+from hamcrest import has_property
 
 from nti.testing.matchers import validly_provides
 from nti.testing.matchers import verifiably_provides
 
 import unittest
+
+from zope import component
+
+from zope.intid.interfaces import IIntIds
 
 from nti.threadable.interfaces import IThreadable
 
@@ -26,7 +33,41 @@ class TestThreadable(unittest.TestCase):
 
     layer = SharedConfiguringTestLayer
 
-    def test_interface(self):
+    def test_model(self):
         threadable = Threadable()
         assert_that(threadable, validly_provides(IThreadable))
         assert_that(threadable, verifiably_provides(IThreadable))
+        
+        assert_that(threadable.isOrWasChildInThread(),
+                    is_(False))
+        
+        assert_that(threadable,
+                    has_property('replies', is_(())))
+        
+        assert_that(threadable,
+                    has_property('referents', is_(())))
+        
+        assert_that(threadable,
+                    has_property('most_recent_reply', is_(none())))
+
+        
+        mock = Threadable()
+        class MockIntIds(object):
+            def getObject(self, unused_doc_id):
+                return mock
+            
+        intids = MockIntIds()
+        component.getGlobalSiteManager().registerUtility(intids, IIntIds)
+        threadable._replies = [1]
+        
+        assert_that(list(threadable.replies),
+                    is_([mock]))
+        
+        assert_that(threadable,
+                    has_property('most_recent_reply', is_(mock)))
+         
+        threadable._referents = [1]
+        assert_that(list(threadable.referents),
+                    is_([mock]))
+        
+        component.getGlobalSiteManager().unregisterUtility(intids, IIntIds)
